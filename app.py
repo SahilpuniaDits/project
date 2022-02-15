7aca13db2e341cd2d8719069437d174bde798a9c
SahilpuniaDits <sahilpuniawins@gmail.com>
2022-02-15 09:52:01 +0530

    buyer_seller

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
+from models.db import app
+from venv import create
 from flask import Flask
 from routes import initialize_routeapi
 from models.db import db,SQLAlchemy,Migrate
 from flask_migrate import Migrate
+from models.buyer import Buyerbid
+import sqlite3
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/apis'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy()
-migrate = Migrate(db)
-db.init_app(app)
-
+con = sqlite3.connect("Buy_sell.db")
+print("Database opened successfully")
+con.close()
+# db.create_all()
+# db.session.commit()
+   
 initialize_routeapi(app)
-# with app.app_context():
-#     db.create_all()
+
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
+    
 
